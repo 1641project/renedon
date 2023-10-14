c637211f6c2db4a1d80d2cb8d56cbfce1723759b
ありす <alice.164@outlook.jp>
2023-10-14 18:38:07 +0900

    Update instance_presenter_spec.rb

--- spec/presenters/instance_presenter_spec.rb
+++ spec/presenters/instance_presenter_spec.rb
@@ -109,7 +109,7 @@ describe InstancePresenter do
       end
 
       it 'defaults to the core mastodon repo URL' do
-        expect(instance_presenter.source_url).to eq('https://github.com/kmycode/mastodon')
+        expect(instance_presenter.source_url).to eq('https://github.com/1641project/renedon')
       end
     end
   end
